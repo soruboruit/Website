056100432d1d4e4ecd86794ad9759340492daf64
Soru <ityouna007@gmail.com>
2023-08-31 02:53:45 +0900

    add

--- app.js
+++ app.js
@@ -9,16 +9,39 @@ const correct = 'そるぼる';
 
 document.getElementById('js.question').textContent = question;
 
-//定数の文字列をHTMLに反映させる
-const buttons = document.getElementsByTagName('button');
+// 定数の文字列をHTMLに反映させる
+
+const $buttons = document.getElementsByTagName('button');
+
 for (let i = 0; i < answer.length; i++) {
-    buttons[i].textContent = answer[i];
+    $buttons[i].textContent = answer[i];
 }
 
-//ボタンをクリックしたら正誤判定
-buttons[i].addEventListener('click', () => {
-    if (correct === buttons[i].textContent) {
-        window.alert('正解！');
+// ボタンをクリックしたら正誤判定
+$buttons[0].addEventListener('click', () => {
+    if (correct === $buttons[0].textContent) { 
+       window.alert('正解！'); 
+    } else {
+        window.alert('不正解！');
+    }
+});
+$buttons[1].addEventListener('click', () => {
+    if (correct === $buttons[1].textContent) { 
+       window.alert('正解！'); 
+    } else {
+        window.alert('不正解！');
+    }
+});
+$buttons[2].addEventListener('click', () => {
+    if (correct === $buttons[2].textContent) { 
+       window.alert('正解！'); 
+    } else {
+        window.alert('不正解！');
+    }
+});
+$buttons[3].addEventListener('click', () => {
+    if (correct === $buttons[3].textContent) { 
+       window.alert('正解！'); 
     } else {
         window.alert('不正解！');
     }
