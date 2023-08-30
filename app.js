dacae9f550140caa769f70f9d99afb3337cb0372
Soru <ityouna007@gmail.com>
2023-08-31 03:14:34 +0900

    fix

--- app.js
+++ app.js
@@ -10,16 +10,14 @@ const correct = 'そるぼる';
 document.getElementById('js.question').textContent = question;
 
 // 定数の文字列をHTMLに反映させる
-
 const $buttons = document.getElementsByTagName('button');
-
 for (let i = 0; i < answer.length; i++) {
     $buttons[i].textContent = answer[i];
 }
 
 // ボタンをクリックしたら正誤判定
 const buttons = document.getElementsByTagName('button');
-//for文を足して繰り返しのパターンを作った
+//for文を足して繰り返しのパターン
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         if (correct === buttons[i].textContent) {
