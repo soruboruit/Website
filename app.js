4477fd24482d00fbb6a86e6692c4d817726dee58
Soru <ityouna007@gmail.com>
2023-08-31 02:28:15 +0900

    add can decide whether it's right or wrong by clicking a button

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ const correct = 'そるぼる';
 
 document.getElementById('js.question').textContent = question;
 
+//定数の文字列をHTMLに反映させる
 const buttons = document.getElementsByTagName('button');
 for (let i = 0; i < answer.length; i++) {
     buttons[i].textContent = answer[i];
 }
+
+//ボタンをクリックしたら正誤判定
+document.getElementsByTagName('buttom')[0].addEventListener('click', () => {
+    if (correct === document.getElementsByTagName('button')[0].textContent) {
+       window.alert('正解！'); 
+    }else {
+        window.alert('不正解！');
+    }
+});
