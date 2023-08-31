5b29a75ecb4871cc6798d3aab3909ec759e4589a
Soru <ityouna007@gmail.com>
2023-09-01 04:34:08 +0900

    add js test

---
+++ Javascript all tests/one.js
@@ -0,0 +1,32 @@
+//変数
+/*
+let name ="soru";
+console.log(name);//出力結果：soru
+ name = "riru";
+ console.log(name);//出力結果：riru
+*/
+
+ //定数
+
+/*
+ const $name ="soru";
+ console.log($name); //出力結果：soru
+ $name ="riru"; //　エラー！　定数は一度代入された値を更新することができない！
+*/
+
+//文字列の中で＄{定数}とすることで、文字列の中に定数や変数を含めることができます。
+/*
+const name = "soruhaha";
+console.log(`${name}です`);
+*/
+
+//if文
+
+//if文を用いると「もし○○ならば●●を行う」という条件分岐が可能になります。ifの後ろに条件式を書き、それが「成り立つ」場合の処理を{ }の中に書きます。
+/*
+const number = 12;
+
+if (number > 10) {
+   console.log('numberは10より大きいです') ;
+}
+*/
