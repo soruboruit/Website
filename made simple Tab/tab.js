4c9aef53ea31eeaa926f8164817f5591a381da7e
Soru <ityouna007@gmail.com>
2023-09-01 02:50:30 +0900

    js

---
+++ made simple Tab/tab.js
@@ -0,0 +1,7 @@
+(() => {
+    const $doc = document;
+    const $tab = $doc.getElementById('js-tab'); 
+    const $nav = $tab.querySelectorAll('[data-nav]');
+
+    console.log('$nav', $nav);
+})();
