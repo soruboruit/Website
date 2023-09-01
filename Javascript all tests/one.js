4e67c52b07298ff578c453f78eb97316ff9a09c6
Soru <ityouna007@gmail.com>
2023-09-01 12:17:45 +0900

    add switch

--- Javascript all tests/one.js
+++ Javascript all tests/one.js
@@ -58,3 +58,24 @@ sigotoSagashi( job_a );
 sigotoSagashi( job_b );
 sigotoSagashi( job_c );
 */
+
+//switch文
+/*
+const n = 4;
+
+switch (n) {
+  case 1:
+    console.log("大吉です");
+    break;
+  case 2:
+    console.log("吉です");
+    break;
+  case 3:
+    console.log("小吉です");
+    break;
+  // どのcaseにも合致しなかったときの処理を追加してください
+  default:
+    console.log("凶です");
+    break;
+}
+*/
